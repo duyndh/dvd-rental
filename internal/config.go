bf92279b3039ae7c75d8a6de7353180bb2436612
Duy Nguyen <ndhduy7798@gmail.com>
2020-02-25 00:38:38 +0700

    Add Config

---
+++ internal/config.go
@@ -0,0 +1,22 @@
+package configs
+
+//Services represents services config.
+type Services struct {
+	Name     string    `yaml:"name,omitempty"`
+	Database *Database `yaml:"database,omitempty"`
+	Cache    *Cache    `yaml:"cache,omitempty"`
+}
+
+//Database represents the database config.
+type Database struct {
+	DBName  string `yaml:"dbName,omitempty"`
+	Timeout int    `yaml:"timeout,omitempty"`
+	PSN     string `yaml:"psn,omitempty"`
+}
+
+//Cache represents the cache config.
+type Cache struct {
+	Addr     string `yaml:"addr,omitempty"`
+	Password string `yaml:"password,omitempty"`
+	CacheKey string `yaml:"cacheKey,omitempty"`
+}

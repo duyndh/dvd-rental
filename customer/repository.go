75068551021dd6233432c5aa377349ea0f6be4fe
Duy Nguyen <ndhduy.7798@gmail.com>
2020-02-25 18:06:01 +0700

    ReStructure Cache and Database

--- customer/repository.go
+++ customer/repository.go
@@ -2,18 +2,45 @@ package customer
 
 import (
 	"github.com/go-pg/pg/v9"
-	"github.com/go-redis/redis/v7"
 )
 
+type CustomerRepository interface {
+	Store(c *Customer) error
+	GetByID(q string) (*Customer, error)
+	Update(c *Customer) error
+	Delete(c *Customer) error
+}
+type CustomerCache interface {
+	Get(id string) (*Customer, error)
+	Set(key string, value interface{}) error
+}
 type customerRepository struct {
 	db *pg.DB
+	cache CustomerCache
+}
+//NewCustomerRepository create a new customer repository.
+func NewCustomerRepository(db *pg.DB, cache CustomerCache) CustomerRepository {
+	return &customerRepository{ db:db, cache: cache }
 }
 
-type customerCache struct {
-	repo Repository
-	cache redis.Client
+func (cr *customerRepository)Store(c *Customer) error {
+	// tx, err := cr.db.Begin()
+	return nil
 }
 
-// func (cr *customerRepository) Store(c *Customer) error {
-// 	return nil
-// }
+
+func (cr *customerRepository)GetByID(q string) (*Customer, error) {
+	// tx, err := cr.db.Begin()
+	return nil,nil
+}
+
+
+func (cr *customerRepository)Update(c *Customer) error {
+	// tx, err := cr.db.Begin()
+	return nil
+}
+
+func (cr *customerRepository)Delete(c *Customer) error {
+	// tx, err := cr.db.Begin()
+	return nil
+}

6bdbfe01d1df692312a8d3253c5016906a1ef7d5
Duy Nguyen <ndhduy7798@gmail.com>
2020-02-25 23:37:35 +0700

    Implement Store, GetByID - Repo

--- customer/repository.go
+++ customer/repository.go
@@ -2,45 +2,64 @@ package customer
 
 import (
 	"github.com/go-pg/pg/v9"
+	"github.com/go-redis/redis/v7"
+	"github.com/ngray1747/dvd-rental/internal/config"
 )
 
-type CustomerRepository interface {
-	Store(c *Customer) error
-	GetByID(q string) (*Customer, error)
-	Update(c *Customer) error
-	Delete(c *Customer) error
-}
-type CustomerCache interface {
-	Get(id string) (*Customer, error)
-	Set(key string, value interface{}) error
+//Cache provides access to customer cache
+type Cache interface {
+	StoreToCache(key string, value Customer) error
+	GetFromCache(key, field string) (*Customer, error)
 }
 type customerRepository struct {
-	db *pg.DB
-	cache CustomerCache
+	cfg   *config.Cache
+	db    *pg.DB
+	cache Cache
 }
+
 //NewCustomerRepository create a new customer repository.
-func NewCustomerRepository(db *pg.DB, cache CustomerCache) CustomerRepository {
-	return &customerRepository{ db:db, cache: cache }
+func NewCustomerRepository(db *pg.DB, cache Cache) Repository {
+	return &customerRepository{db: db, cache: cache}
 }
 
-func (cr *customerRepository)Store(c *Customer) error {
+func (cr *customerRepository) Store(c *Customer) error {
+	tx, err := cr.db.Begin()
+	if err != nil {
+		return err
+	}
+	// Rollback tx on error.
+	defer tx.Close()
+	err = tx.Insert(c)
+	if err != nil {
+		return err
+	}
+	err = cr.cache.StoreToCache(cr.cfg.CacheKey, *c)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (cr *customerRepository) GetByID(id string) (*Customer, error) {
+	//* Get data from cache first
+	customer, err := cr.cache.GetFromCache(cr.cfg.CacheKey, id)
+	if err != nil && err != redis.Nil {
+		return nil, err
+	}else if err == redis.Nil {
+		customer = &Customer{ID: id}
+		if err := cr.db.Select(customer); err != nil {
+			return nil, err
+		}
+	}
+	return customer, nil
+}
+
+func (cr *customerRepository) Update(c *Customer) error {
 	// tx, err := cr.db.Begin()
 	return nil
 }
 
-
-func (cr *customerRepository)GetByID(q string) (*Customer, error) {
-	// tx, err := cr.db.Begin()
-	return nil,nil
-}
-
-
-func (cr *customerRepository)Update(c *Customer) error {
-	// tx, err := cr.db.Begin()
-	return nil
-}
-
-func (cr *customerRepository)Delete(c *Customer) error {
+func (cr *customerRepository) Delete(c *Customer) error {
 	// tx, err := cr.db.Begin()
 	return nil
 }

18e8b62465072317e51c99233973aac2acc21948
Duy Nguyen <ndhduy.7798@gmail.com>
2020-02-26 18:29:46 +0700

    Update customer service

--- customer/service.go
+++ customer/service.go
@@ -8,30 +8,63 @@ import (
 var (
 	errInvalidArgument = errors.New("invalid argument(s)")
 )
+
 //Service describe customer business
 type Service interface {
-	Register(ctx context.Context, name, address string) (string, error)
+	//Register customer
+	Register(ctx context.Context, name, address string) error
+	//Customer rent a dvd
 	Rent(ctx context.Context, id int) error
+	//Customer buys a dvd
 	Buy(ctx context.Context, id int) error
+	//Customer returns borrowed dvd
 	Return(ctx context.Context, id int) error
 }
 
-//NewService init customer's service interface
-func NewService() Service {
+//NewService return customerService with all expected function
+func NewService(customerRepo Repository) Service {
 	var svc Service
+	{
+		svc = NewCustomerService(customerRepo)
+	}
 	return svc
 }
 
 //customerService implement Service interface
-type customerService struct{}
-
-func newCustomerService() customerService {
-	return customerService{}
+type customerService struct{
+	repo Repository
 }
 
-func (c *customerService) Register(ctx context.Context, name, address string) (string, error) {
+//NewCustomerService init customer's service interface
+func NewCustomerService(customerRepo Repository) Service {
+	return customerService{repo: customerRepo}
+}
+
+func (c *customerService) Register(ctx context.Context, name, address string) error {
 	if name == "" || address == "" {
-		return "", errInvalidArgument
+		return errInvalidArgument
+	}
+	customer, err := NewCustomer(name, address)
+	if err != nil {
+		return err
 	}
 	
+	if err = c.repo.Store(customer); err != nil {
+		return err
+	}
+	return nil
+
+}
+
+
+func (c *customerService) Rent(ctx context.Context, id int) error {
+	return nil
+}
+
+func (c *customerService) Buy(ctx context.Context, id int) error {
+	return nil
+}
+
+func (c *customerService) Return(ctx context.Context, id int) error {
+	return nil
 }

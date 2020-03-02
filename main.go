cc839f37d8692859e13be59ed8a90d54e238acba
Duy Nguyen <ndhduy.7798@gmail.com>
2020-03-02 23:28:02 +0700

    Update main

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import (
 	"os"
 
 	"github.com/go-kit/kit/log"
+	"github.com/go-redis/redis/v7"
 	"github.com/ngray1747/dvd-rental/customer"
+	"github.com/ngray1747/dvd-rental/internal/config"
 )
 
 func main() {
@@ -12,8 +14,16 @@ func main() {
 	logger = log.NewLogfmtLogger(log.NewSyncWriter(os.Stderr))
 	logger = log.With(logger, "ts", log.DefaultTimestamp)
 
-	
+	//Get app config
+	cfg, err := config.Load("dev")
+	if err != nil {
+		panic(err)
+	}
+	// cli, err := redis.NewClient(&redis.Options{
+		
+	// })
+	// customerCache := customer.NewCacheClient()
 	var (
-		customer = customer.NewCustomerRepository()
+		// customer = customer.NewCustomerRepository()
 	)
 }

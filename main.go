3550bd0441e88443e248c341fd38450f19980979
Duy Nguyen <ndhduy7798@gmail.com>
2020-03-03 23:00:11 +0700

    complemete main

--- main.go
+++ main.go
@@ -2,16 +2,19 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/go-kit/kit/log"
+	kitprometheus "github.com/go-kit/kit/metrics/prometheus"
 	"github.com/go-pg/pg/v9"
 	"github.com/go-redis/redis/v7"
 	"github.com/ngray1747/dvd-rental/customer"
 	"github.com/ngray1747/dvd-rental/internal/config"
-	kitprometheus "github.com/go-kit/kit/metrics/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
 	stdprometheus "github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func getConf(name string, services []config.Service) *config.Service {
@@ -37,28 +40,24 @@ func main() {
 		panic(err)
 	}
 
-	svcName := os.Getenv("SERVICE_NAME")
-	if len(svcName) == 0 {
-		panic("missing SERVICE_NAME")
-	}
 	//Get Customer configuration
-	srvCfg := getConf("customer", cfg.Services)
+	customerCfg := getConf("customer", cfg.Services)
 	cacheCli := redis.NewClient(&redis.Options{
-		Addr:     srvCfg.Cache.Addr,
-		Password: srvCfg.Cache.Password,
+		Addr:     customerCfg.Cache.Addr,
+		Password: customerCfg.Cache.Password,
 		DB:       0,
 	})
 	defer cacheCli.Close()
 	cacheRepo := customer.NewCacheClient(cacheCli)
-	pgConnectionString, err := pg.ParseURL(srvCfg.Database.PSN)
+	pgConnectionString, err := pg.ParseURL(customerCfg.Database.PSN)
 	if err != nil {
 		panic(err)
 	}
 	db := pg.Connect(pgConnectionString)
 	defer db.Close()
-	
+
 	fielKeys := []string{"method"}
-	
+
 	customerRepo := customer.NewCustomerRepository(db, cacheRepo)
 	var cs customer.Service
 	cs = customer.NewService(customerRepo)
@@ -67,23 +66,49 @@ func main() {
 		kitprometheus.NewCounterFrom(stdprometheus.CounterOpts{
 			Namespace: "api",
 			Subsystem: "customer_service",
-			Name: "request_count",
-			Help: "Number of requests received",
+			Name:      "request_count",
+			Help:      "Number of requests received",
 		}, fielKeys),
 		kitprometheus.NewSummaryFrom(stdprometheus.SummaryOpts{
 			Namespace: "api",
 			Subsystem: "customer_service",
-			Name: "request_latency_microseconds",
-			Help: "Request duration",
+			Name:      "request_latency_microseconds",
+			Help:      "Request duration",
 		}, fielKeys),
 		cs,
 	)
-	
-	fmt.Printf("%+v", cfg.Services[0].Cache)
 
-	// customerCache := customer.NewCacheClient()
-	var (
-	// customer = customer.NewCustomerRepository()
-	)
+	mux := http.NewServeMux()
 
+	mux.Handle("/customer/v1/", customer.MakeHandler(cs, logger))
+
+	http.Handle("/metrics", promhttp.Handler())
+
+	errs := make(chan error, 2)
+
+	go func() {
+		logger.Log("transport", "http", "address", "localhost:9999", "msg", "listening")
+		errs <- http.ListenAndServe("localhost:9999", nil)
+	}()
+	go func() {
+		c := make(chan os.Signal)
+		signal.Notify(c, syscall.SIGINT)
+		errs <- fmt.Errorf("%s", <-c)
+	}()
+
+	logger.Log("shutting down", <-errs)
+}
+
+func accessControl(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type")
+		
+		if r.Method == "OPTIONS" {
+			return
+		}
+
+		h.ServeHTTP(w,r)
+	})
 }

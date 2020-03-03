958920b07a4f3d356b6af723603ed7eff30334f2
Duy Nguyen <ndhduy.7798@gmail.com>
2020-03-03 21:14:26 +0700

    Update main file

--- go.mod
+++ go.mod
@@ -8,4 +8,5 @@ require (
 	github.com/go-redis/redis/v7 v7.2.0
 	github.com/google/uuid v1.1.1
 	github.com/gorilla/mux v1.7.3
+	gopkg.in/yaml.v2 v2.2.4
 )
--- main.go
+++ main.go
@@ -1,13 +1,30 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/go-kit/kit/log"
+	"github.com/go-pg/pg/v9"
 	"github.com/go-redis/redis/v7"
 	"github.com/ngray1747/dvd-rental/customer"
 	"github.com/ngray1747/dvd-rental/internal/config"
+	kitprometheus "github.com/go-kit/kit/metrics/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	stdprometheus "github.com/prometheus/client_golang/prometheus"
 )
+
+func getConf(name string, services []config.Service) *config.Service {
+	if len(services) == 0 {
+		panic("service is not configured")
+	}
+	for _, service := range services {
+		if service.Name == name {
+			return &service
+		}
+	}
+	panic("service does not configured")
+}
 
 func main() {
 	var logger log.Logger
@@ -19,11 +36,54 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	// cli, err := redis.NewClient(&redis.Options{
-		
-	// })
+
+	svcName := os.Getenv("SERVICE_NAME")
+	if len(svcName) == 0 {
+		panic("missing SERVICE_NAME")
+	}
+	//Get Customer configuration
+	srvCfg := getConf("customer", cfg.Services)
+	cacheCli := redis.NewClient(&redis.Options{
+		Addr:     srvCfg.Cache.Addr,
+		Password: srvCfg.Cache.Password,
+		DB:       0,
+	})
+	defer cacheCli.Close()
+	cacheRepo := customer.NewCacheClient(cacheCli)
+	pgConnectionString, err := pg.ParseURL(srvCfg.Database.PSN)
+	if err != nil {
+		panic(err)
+	}
+	db := pg.Connect(pgConnectionString)
+	defer db.Close()
+	
+	fielKeys := []string{"method"}
+	
+	customerRepo := customer.NewCustomerRepository(db, cacheRepo)
+	var cs customer.Service
+	cs = customer.NewService(customerRepo)
+	cs = customer.NewLoggingSerivce(log.With(logger, "service", "customer"), cs)
+	cs = customer.NewInstrumentService(
+		kitprometheus.NewCounterFrom(stdprometheus.CounterOpts{
+			Namespace: "api",
+			Subsystem: "customer_service",
+			Name: "request_count",
+			Help: "Number of requests received",
+		}, fielKeys),
+		kitprometheus.NewSummaryFrom(stdprometheus.SummaryOpts{
+			Namespace: "api",
+			Subsystem: "customer_service",
+			Name: "request_latency_microseconds",
+			Help: "Request duration",
+		}, fielKeys),
+		cs,
+	)
+	
+	fmt.Printf("%+v", cfg.Services[0].Cache)
+
 	// customerCache := customer.NewCacheClient()
 	var (
-		// customer = customer.NewCustomerRepository()
+	// customer = customer.NewCustomerRepository()
 	)
+
 }

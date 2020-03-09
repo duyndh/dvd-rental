d142abebc6cab73b25edc452f12d8123926810fd
Duy Nguyen <ndhduy.7798@gmail.com>
2020-03-09 18:30:26 +0700

    Init Service Test

---
+++ customer/service_test.go
@@ -0,0 +1,14 @@
+package service_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/ngray1747/dvd-rental/customer/mocks"
+)
+
+func TestRegister(t *testing.T) {
+	ctx := context.Context
+	// svc 
+	
+}

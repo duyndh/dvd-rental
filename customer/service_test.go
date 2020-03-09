44567eb84ba426487af26a55d38949aafb1369ee
Duy Nguyen <ndhduy7798@gmail.com>
2020-03-10 00:20:22 +0700

    Add test case

--- customer/service_test.go
+++ customer/service_test.go
@@ -1,14 +1,50 @@
-package service_test
+package customer_test
 
 import (
 	"context"
 	"testing"
 
+	"github.com/go-kit/kit/log"
+	"github.com/go-kit/kit/metrics/discard"
+	"github.com/ngray1747/dvd-rental/customer"
 	"github.com/ngray1747/dvd-rental/customer/mocks"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 )
 
 func TestRegister(t *testing.T) {
-	ctx := context.Context
-	// svc 
-	
+	assert := assert.New(t)
+	ctx := context.Background()
+	repo := &mocks.Repository{}
+	svc := customer.NewService(repo, log.NewNopLogger(), discard.NewCounter(), discard.NewHistogram())
+
+	type args struct {
+		name    string
+		address string
+	}
+	cases := []struct {
+		name    string
+		args    args
+		wantErr bool
+		mock    func()
+	}{
+		{
+			name: "OK",
+			args: args{
+				name:    "Duynguyen",
+				address: "1102 Truong Sa Street",
+			},
+			wantErr: false,
+			mock: func() {
+				repo.On("Store", mock.Anything).Return(nil)
+			},
+		},
+	}
+	for _, v := range cases {
+		t.Run(v.name, func(t *testing.T) {
+			v.mock()
+			err := svc.Register(ctx, v.args.name, v.name)
+			assert.Equal(v.wantErr, err != nil)
+		})
+	}
 }
--- go.mod
+++ go.mod
@@ -14,6 +14,7 @@ require (
 	github.com/prometheus/client_golang v1.3.0
 	github.com/sony/gobreaker v0.4.1
 	github.com/spf13/viper v1.6.2
+	github.com/stretchr/testify v1.4.0
 	github.com/vmihailenco/msgpack v4.0.4+incompatible
 	golang.org/x/time v0.0.0-20191024005414-555d28b269f0
 )

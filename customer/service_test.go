68a897a4588f34496fcba6126e967ca38805f09f
Duy Nguyen <ndhduy.7798@gmail.com>
2020-03-10 19:44:09 +0700

    Add service test case

--- customer/service_test.go
+++ customer/service_test.go
@@ -2,6 +2,8 @@ package customer_test
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"testing"
 
 	"github.com/go-kit/kit/log"
@@ -15,9 +17,8 @@ import (
 func TestRegister(t *testing.T) {
 	assert := assert.New(t)
 	ctx := context.Background()
-	repo := &mocks.Repository{}
+	repo := new(mocks.Repository)
 	svc := customer.NewService(repo, log.NewNopLogger(), discard.NewCounter(), discard.NewHistogram())
-
 	type args struct {
 		name    string
 		address string
@@ -28,23 +29,56 @@ func TestRegister(t *testing.T) {
 		wantErr bool
 		mock    func()
 	}{
+		// {
+		// 	name: "OK",
+		// 	args: args{
+		// 		name:    "Duynguyen",
+		// 		address: "1102 Truong Sa Street",
+		// 	},
+		// 	wantErr: false,
+		// 	mock: func() {
+		// 		repo.On("Store", mock.Anything).Return(nil).Once()
+		// 	},
+		// },
+		// {
+		// 	name: "missing name",
+		// 	args: args{
+		// 		address: "1102 Truong Sa Street",
+		// 	},
+		// 	wantErr: true,
+		// 	mock: func() {
+		// 		repo.On("Store", mock.Anything).Return(nil).Once()
+		// 	},
+		// },
 		{
-			name: "OK",
+			name: "missing address",
+			args: args{
+				name: "Duynguyen",
+			},
+			wantErr: true,
+			mock: func() {
+				repo.On("Store", mock.Anything).Return(nil).Once()
+			},
+		},
+		{
+			name: "store failed",
 			args: args{
 				name:    "Duynguyen",
 				address: "1102 Truong Sa Street",
 			},
-			wantErr: false,
+			wantErr: true,
 			mock: func() {
-				repo.On("Store", mock.Anything).Return(nil)
+				repo.On("Store", mock.Anything).Return(errors.New("store failed")).Once()
 			},
 		},
 	}
 	for _, v := range cases {
 		t.Run(v.name, func(t *testing.T) {
 			v.mock()
-			err := svc.Register(ctx, v.args.name, v.name)
-			assert.Equal(v.wantErr, err != nil)
+			err := svc.Register(ctx, v.args.name, v.args.address)
+			fmt.Println(err)
+			// repo.AssertCalled(t, "Store")
+			assert.Equalf(v.wantErr, err != nil, "name: %v , wantErr %v, got %v , err ", v.name, v.wantErr, err != nil, err)
 		})
 	}
 }

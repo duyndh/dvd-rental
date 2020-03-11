2adefbb66b193df5a2d69bfe71b0cd19762a795a
Duy Nguyen <ndhduy7798@gmail.com>
2020-03-12 00:56:15 +0700

    Restructure repository

--- customer/database/repository.go
+++ customer/database/repository.go
@@ -1,15 +1,16 @@
-package customer
+package database
 
 import (
 	"github.com/go-pg/pg/v9"
 	"github.com/go-redis/redis/v7"
+	"github.com/ngray1747/dvd-rental/customer"
 	"github.com/ngray1747/dvd-rental/internal/config"
 )
 
 //Cache provides access to customer cache
 type Cache interface {
-	StoreToCache(key string, value Customer) error
-	GetFromCache(key, field string) (*Customer, error)
+	StoreToCache(key string, value customer.Customer) error
+	GetFromCache(key, field string) (*customer.Customer, error)
 	RemoveFromCache(key, field string) error
 }
 type customerRepository struct {
@@ -19,11 +20,11 @@ type customerRepository struct {
 }
 
 //NewCustomerRepository create a new customer repository.
-func NewCustomerRepository(cfg *config.Cache, db *pg.DB, cache Cache) Repository {
+func NewCustomerRepository(cfg *config.Cache, db *pg.DB, cache Cache) customer.Repository {
 	return &customerRepository{cfg: cfg, db: db, cache: cache}
 }
 
-func (cr *customerRepository) Store(c *Customer) error {
+func (cr *customerRepository) Store(c *customer.Customer) error {
 	tx, err := cr.db.Begin()
 	if err != nil {
 		return err
@@ -33,33 +34,33 @@ func (cr *customerRepository) Store(c *Customer) error {
 	if err := tx.Insert(c); err != nil {
 		return err
 	}
-	
+
 	if err := cr.cache.StoreToCache(cr.cfg.CacheKey, *c); err != nil {
 		return err
 	}
 	return tx.Commit()
 }
 
-func (cr *customerRepository) GetByID(id string) (*Customer, error) {
+func (cr *customerRepository) GetByID(id string) (*customer.Customer, error) {
 	//* Get data from cache first
-	customer, err := cr.cache.GetFromCache(cr.cfg.CacheKey, id)
+	cus, err := cr.cache.GetFromCache(cr.cfg.CacheKey, id)
 	if err != nil && err != redis.Nil {
 		return nil, err
 	} else if err == redis.Nil {
-		customer = &Customer{ID: id}
+		cus = &customer.Customer{ID: id}
 		// Get from database
-		if err := cr.db.Select(customer); err != nil {
+		if err := cr.db.Select(cus); err != nil {
 			return nil, err
 		}
 		// Set back to cache
-		if err = cr.cache.StoreToCache(cr.cfg.CacheKey, *customer); err != nil {
+		if err = cr.cache.StoreToCache(cr.cfg.CacheKey, *cus); err != nil {
 			return nil, err
 		}
 	}
-	return customer, nil
+	return cus, nil
 }
 
-func (cr *customerRepository) Update(c *Customer) error {
+func (cr *customerRepository) Update(c *customer.Customer) error {
 	tx, err := cr.db.Begin()
 	if err != nil {
 		return err
@@ -78,7 +79,7 @@ func (cr *customerRepository) Update(c *Customer) error {
 	return nil
 }
 
-func (cr *customerRepository) Delete(c *Customer) error {
+func (cr *customerRepository) Delete(c *customer.Customer) error {
 	tx, err := cr.db.Begin()
 	if err != nil {
 		return err

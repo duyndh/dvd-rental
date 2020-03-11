9d203a8061762177c33043ba7811e75b554a77d9
Duy Nguyen <ndhduy.7798@gmail.com>
2020-03-11 18:29:58 +0700

    Init test

---
+++ customer/cache_test.go
@@ -0,0 +1,37 @@
+package cache_test
+
+import (
+    "log"
+    "fmt"
+    "testing"
+
+    "github.com/go-redis/redis/v7"
+	"github.com/ory/dockertest"
+)
+
+func TestMain(t *testing.M) {
+    var db *redis.Client
+    var err error
+    pool, err := dockertest.NewPool("")
+    if err != nil {
+        log.Fatalf("Could not connect to docker: %s", err)
+    }
+
+    resource, err := pool.Run("redis", "3.2", nil)
+    if err != nil {
+        log.Fatalf("Could not start resource: %s", err)
+    }
+
+    if err = pool.Retry(func() error {
+        db = redis.NewClient(&redis.Options{
+            Addr: fmt.Sprintf("localhost:%s", resource.GetPort("6379/tcp")),
+        })
+
+        return db.Ping().Err()
+    }); err != nil {
+        log.Fatalf("Could not connect to docker: %s", err)
+    }
+
+    // When you're done, kill and remove the container
+    err = pool.Purge(resource)
+}

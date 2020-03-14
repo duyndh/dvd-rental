b2ae813077eee9ced76c4c997cde6aa1f4635706
Duy Nguyen <ndhduy7798@gmail.com>
2020-03-14 23:06:26 +0700

    restructure main file

--- main.go
+++ main.go
@@ -1,21 +1,29 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
+	"net"
 	"net/http"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 
 	"github.com/go-kit/kit/log"
 	"github.com/go-kit/kit/metrics"
 	kitprometheus "github.com/go-kit/kit/metrics/prometheus"
+	kitgrpc "github.com/go-kit/kit/transport/grpc"
 	"github.com/go-pg/pg/v9"
 	"github.com/go-redis/redis/v7"
 	"github.com/ngray1747/dvd-rental/customer"
-	"github.com/ngray1747/dvd-rental/customer/cache"
-	"github.com/ngray1747/dvd-rental/customer/database"
+	customerCache "github.com/ngray1747/dvd-rental/customer/cache"
+	customerRepo "github.com/ngray1747/dvd-rental/customer/repository"
+	"github.com/ngray1747/dvd-rental/dvd"
+	dvdCache "github.com/ngray1747/dvd-rental/dvd/cache"
+	dvdPB "github.com/ngray1747/dvd-rental/dvd/pb"
+	dvdRepo "github.com/ngray1747/dvd-rental/dvd/repository"
 	"github.com/ngray1747/dvd-rental/internal/config"
 	stdopentracing "github.com/opentracing/opentracing-go"
 	zipkinot "github.com/openzipkin-contrib/zipkin-go-opentracing"
@@ -23,30 +31,39 @@ import (
 	zipkinhttp "github.com/openzipkin/zipkin-go/reporter/http"
 	stdprometheus "github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"google.golang.org/grpc"
 )
 
-func getConf(name string, services []config.Service) *config.Service {
+var (
+	errServiceNotConfigurated = errors.New("service is not configured")
+	errServiceNotFound        = errors.New("service not found")
+)
+
+func getConf(name string, services []config.Service) (*config.Service, error) {
 	if len(services) == 0 {
-		panic("service is not configured")
+		return nil, errServiceNotConfigurated
 	}
 	for _, service := range services {
 		if service.Name == name {
-			return &service
+			return &service, nil
 		}
 	}
-	panic("service does not configured")
+	return nil, errServiceNotFound
 }
 
 func main() {
 	fs := flag.NewFlagSet("dvd_rental", flag.ExitOnError)
 	var (
 		httpAddr      = fs.String("httpAddr", ":9999", "Http server address")
+		grpcAddr      = fs.String("grpcAddr", ":8888", "GRPC server address")
 		zipkinAddr    = fs.String("zipkinAddr", "", "Zipkin tracer address")
 		dbUserName    = fs.String("dbUserName", "my-user", "Postgresql username")
 		dbPassword    = fs.String("dbPassword", "password123", "Postgresql password")
 		dbAddr        = fs.String("dbHost", "", "Postgresql host")
 		redisAddr     = fs.String("redisAddr", "", "Redis cache address")
 		redisPassword = fs.String("redisPassword", "", "Redis cache password")
+		svc           = fs.String("service", "", "Service name")
+		namespace     = fs.String("namespace", "", "Service namespace")
 	)
 	fs.Parse(os.Args[1:])
 
@@ -92,30 +109,22 @@ func main() {
 	if *redisAddr == "" {
 		panic("Redis configuration required")
 	}
-	customerCfg := getConf("customer", cfg.Services)
 	cacheCli := redis.NewClient(&redis.Options{
 		Addr:     *redisAddr,
 		Password: *redisPassword,
 		DB:       0,
 	})
 	defer cacheCli.Close()
+
 	if *dbAddr == "" || *dbUserName == "" || *dbPassword == "" {
 		panic("Database configuration required")
 	}
-	cacheRepo := cache.NewCacheClient(cacheCli)
-	db := pg.Connect(&pg.Options{
-		Addr:     *dbAddr,
-		User:     *dbUserName,
-		Password: *dbPassword,
-		Database: customerCfg.Database.DBName,
-	})
-	defer db.Close()
 
 	var historgram metrics.Histogram
 	{
 		historgram = kitprometheus.NewSummaryFrom(stdprometheus.SummaryOpts{
-			Namespace: "api",
-			Subsystem: "customer_service",
+			Namespace: *namespace,
+			Subsystem: *svc,
 			Name:      "request_latency_microseconds",
 			Help:      "Request duration",
 		}, []string{"method", "success"})
@@ -123,29 +132,74 @@ func main() {
 	var counter metrics.Counter
 	{
 		counter = kitprometheus.NewCounterFrom(stdprometheus.CounterOpts{
-			Namespace: "api",
-			Subsystem: "customer_service",
+			Namespace: *namespace,
+			Subsystem: *svc,
 			Name:      "request_count",
 			Help:      "Number of requests received",
 		}, []string{"method"})
 	}
-	customerRepo := database.NewCustomerRepository(customerCfg.Cache, db, cacheRepo)
-	var cs customer.Service
-	cs = customer.NewService(customerRepo, logger, counter, historgram)
-	customerEndpoint := customer.NewCustomerEndpoint(cs, tracer)
-	// customerHandler = customer.NewHTTPHadne
-	mux := http.NewServeMux()
 
-	mux.Handle("/customer/v1/", customer.MakeHandler(customerEndpoint, logger, tracer))
-
-	http.Handle("/", accessControl(mux))
 	http.Handle("/metrics", promhttp.Handler())
+	var grpcServer *grpc.Server
+	switch *svc {
+	case "customer":
+		svcCfg, err := getConf("customer", cfg.Services)
+		if err != nil {
+			logger.Log("get svc config error: ", err)
+			os.Exit(1)
+		}
+		cacheRepo := customerCache.NewCacheClient(cacheCli)
+
+		db := initDB(*dbAddr, *dbUserName, *dbPassword, svcCfg.Database.DBName)
+		defer db.Close()
+		repo := customerRepo.NewCustomerRepository(svcCfg.Cache, db, cacheRepo)
+
+		var cs customer.Service
+		cs = customer.NewService(repo, logger, counter, historgram)
+		customerEndpoint := customer.NewCustomerEndpoint(cs, tracer)
+
+		mux := http.NewServeMux()
+		http.Handle("/", accessControl(mux))
+		mux.Handle("/customer/v1/", customer.MakeHandler(customerEndpoint, logger, tracer))
+		break
+	case "dvd":
+		svcCfg, err := getConf("dvd", cfg.Services)
+		if err != nil {
+			logger.Log("get svc config error: ", err)
+			os.Exit(1)
+		}
+		cacheRepo := dvdCache.NewCacheClient(cacheCli)
+		db := initDB(*dbAddr, *dbUserName, *dbPassword, svcCfg.Database.DBName)
+		defer db.Close()
+		repo := dvdRepo.NewDVDRepository(svcCfg.Cache, db, cacheRepo)
+		var dvdSrv dvd.Service
+		dvdSrv = dvd.NewDVDService(repo)
+		dvdEndpoint := dvd.NewDVDEndpoint(dvdSrv, tracer)
+		dvdGRPCServer := dvd.NewGRPCServer(dvdEndpoint, tracer, logger)
+
+		grpcServer = grpc.NewServer(grpc.UnaryInterceptor(kitgrpc.Interceptor))
+		dvdPB.RegisterDVDRentalServer(grpcServer, dvdGRPCServer)
+		// grpcServer.Serve(listener)
+		break
+	default:
+		break
+	}
 
 	errs := make(chan error, 2)
 
 	go func() {
-		logger.Log("transport", "http", "address", ":9999", "msg", "listening")
-		errs <- http.ListenAndServe(*httpAddr, nil)
+		if strings.ToUpper(*namespace) == "API" {
+			logger.Log("transport", "http", "address", *httpAddr, "msg", "listening")
+			errs <- http.ListenAndServe(*httpAddr, nil)
+		}else if strings.ToUpper(*namespace) == "SVC" {
+			listener, err := net.Listen("tcp", *grpcAddr)
+			if err != nil {
+				logger.Log("net config error: ", err)
+				os.Exit(1)
+			}
+			logger.Log("transport", "GRPC", "address", *grpcAddr, "msg", "listening")
+			errs <- grpcServer.Serve(listener)
+		}
 	}()
 	go func() {
 		c := make(chan os.Signal)
@@ -167,5 +221,14 @@ func accessControl(h http.Handler) http.Handler {
 		}
 
 		h.ServeHTTP(w, r)
+	})
+}
+
+func initDB(addr, username, password, database string) *pg.DB {
+	return pg.Connect(&pg.Options{
+		Addr:     addr,
+		User:     username,
+		Password: password,
+		Database: database,
 	})
 }

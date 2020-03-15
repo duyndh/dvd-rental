34dd714528a2da5e84d62b744dda5f7d0ce95533
Duy Nguyen <ndhduy7798@gmail.com>
2020-03-15 22:09:32 +0700

    Update service

--- dvd/service.go
+++ dvd/service.go
@@ -3,22 +3,35 @@ package dvd
 import (
 	"context"
 	"errors"
+
+	"github.com/go-kit/kit/log"
+	"github.com/go-kit/kit/metrics"
 )
 
 var (
 	errInvalidDVDName = errors.New("invalid dvd name")
 )
+
 type Service interface {
 	CreateDVD(ctx context.Context, name string) error
 }
 
-
 type dvdService struct {
-	repo  Repository
+	repo Repository
 }
 
 func NewDVDService(dvdRepo Repository) Service {
 	return &dvdService{repo: dvdRepo}
+}
+
+func NewService(dvdRepo Repository, logger log.Logger, counter metrics.Counter, histogram metrics.Histogram) Service {
+	var dvdService Service
+	{
+		dvdService = NewDVDService(dvdRepo)
+		dvdService = NewLoggerMiddleware(logger)(dvdService)
+		dvdService = NewMetrictMiddleware(counter, histogram)(dvdService)
+	}
+	return dvdService
 }
 
 func (d *dvdService) CreateDVD(ctx context.Context, name string) error {

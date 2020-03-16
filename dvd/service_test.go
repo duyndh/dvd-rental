bcc68572414e8de04499e0e09cccc57ba249a127
Duy Nguyen <ndhduy.7798@gmail.com>
2020-03-16 17:08:08 +0700

    Add dvd service test

---
+++ dvd/service_test.go
@@ -0,0 +1,66 @@
+package dvd_test
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/go-kit/kit/log"
+	"github.com/go-kit/kit/metrics/discard"
+	"github.com/ngray1747/dvd-rental/dvd"
+	"github.com/ngray1747/dvd-rental/dvd/mocks"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestRegister(t *testing.T) {
+	assert := assert.New(t)
+	ctx := context.Background()
+	repo := new(mocks.Repository)
+	svc := dvd.NewService(repo, log.NewNopLogger(), discard.NewCounter(), discard.NewHistogram())
+	type args struct {
+		name    string
+	}
+	cases := []struct {
+		name    string
+		args    args
+		wantErr bool
+		mock    func()
+	}{
+		{
+			name: "OK",
+			args: args{
+				name:    "Title 1",
+			},
+			wantErr: false,
+			mock: func() {
+				repo.On("Store", mock.Anything).Return(nil).Once()
+			},
+		},
+		{
+			name: "missing name",
+			args: args{
+				name: "",
+			},
+			wantErr: true,
+			mock: func() {},
+		},
+		{
+			name: "store failed",
+			args: args{
+				name:    "Title 2",
+			},
+			wantErr: true,
+			mock: func() {
+				repo.On("Store", mock.Anything).Return(errors.New("store failed")).Once()
+			},
+		},
+	}
+	for _, v := range cases {
+		t.Run(v.name, func(t *testing.T) {
+			v.mock()
+			err := svc.CreateDVD(ctx, v.args.name)
+			assert.Equalf(v.wantErr, err != nil, "name: %v , wantErr %v, got %v , err ", v.name, v.wantErr, err != nil, err)
+		})
+	}
+}
